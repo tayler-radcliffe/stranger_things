7111bf9e2cf427648ece41735cb17ac70f9e6473
ElegantMrS <josephjumber@gmail.com>
2021-02-16 21:04:12 -0500

    Testing

--- src/components/Title.js
+++ src/components/Title.js
@@ -8,7 +8,7 @@ class Title extends PureComponent {
       Stranger's Things
     </h1>
     <h5>
-      Buy and sell items from people around the world.
+      Buy and sell items from people around the world. Hello
     </h5>
   </div>
   }
