c5bfb2b2fc7ba106c94027ed5b7cc842f641b9c8
Your Name <your_email@real_email.edu>
2021-02-16 20:26:32 -0500

    Adding

---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+
+ 
+class Title extends PureComponent {
+  render() {
+  return <div id="title">
+    <h1>
+      Stranger's Things
+    </h1>
+    <h5>
+      Buy and sell items from people around the world.
+    </h5>
+  </div>
+  }
+}
+
+export default Title;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-/* js code here /*
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+    Title,
+} from './components';
+
+const App = () => {
+
+    return <div className="app">
+
+        <Title />
+
+    </div>
+
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app'),
+  )
